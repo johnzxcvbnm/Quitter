1bf54afc262af7e3dac344df6ff3209c7e195f9d
John Kusching <kimshrum@Kims-Air.home>
2018-08-10 14:31:36 -0500

    Added some basic seed data in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Postgres commands to create tables and add basic seed data
+
+# CREATE TABLE users (id SERIAL, user_name VARCHAR(32), password VARCHAR(32), avatar TEXT);
+# CREATE TABLE posts (id SERIAL, post_content TEXT, image TEXT, user_id INT);
+
+# INSERT INTO posts (post_content, image, user_id) VALUES ('This post is way too', 'https://i.ytimg.com/vi/_37IuCIaMFA/maxresdefault.jpg', 1), ('Is this a cat?', 'https://i.ytimg.com/vi/_37IuCIaMFA/maxresdefault.jpg', 1), ('What is this app abo', 'https://i.ytimg.com/vi/_37IuCIaMFA/maxresdefault.jpg', 2), ('I was going to go to the gym, but I decided to go to Dairy Queen Instead', 'https://i.ytimg.com/vi/_37IuCIaMFA/maxresdefault.jpg', 3);
+
+# INSERT INTO users (user_name, password, avatar) VALUES ('John_K', '1234', 'https://vignette.wikia.nocookie.net/parody/images/e/e2/Super_Secret.jpg/revision/latest/scale-to-width-down/400?cb=20180120111247'), ('Jordan', '5678', 'https://vignette.wikia.nocookie.net/parody/images/e/e2/Super_Secret.jpg/revision/latest/scale-to-width-down/400?cb=20180120111247'), ('Sean', 'abc', 'https://vignette.wikia.nocookie.net/parody/images/e/e2/Super_Secret.jpg/revision/latest/scale-to-width-down/400?cb=20180120111247');
