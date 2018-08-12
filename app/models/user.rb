d76b49ed57a660e48e26761ae1ce8259bc1c50e4
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2018-08-12 13:51:33 -0400

    users can see their posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,40 +2,77 @@ class User
   DB = PG.connect({ :host => "localhost", :port => 5432, :dbname => 'quitter_app_development'})
 
   def self.all
-    results = DB.exec("SELECT * FROM users;")
-    return results.map do |result|
-      {
+    results = DB.exec(
+        <<-SQL
+          SELECT
+           users.*,
+           posts.id AS post_id,
+           posts.post_content, posts.user_id
+          FROM users
+          LEFT JOIN posts
+            ON users.id = posts.user_id
+          ORDER BY users.id;
+        SQL
+    )
+    users = []
+    last_id = nil
+    results.each do |result|
+      if result["id"] != last_id
+        users.push({
         "id" => result["id"].to_i,
         "user_name" => result["user_name"],
         "password" => result["password"],
-        "avatar" => result["avatar"]
-      }
+        "avatar" => result["avatar"],
+        "post_id" => result["post_id"],
+        "posts" => []
+        })
+        last_id = result["id"]
+      end
+      if result["post_id"]
+        users.last["posts"].push({
+          "id" => result["post_id"].to_i,
+          "post_content" => result["post_content"],
+          "image" => result["image"],
+          "user_id" => result["user_id"].to_i
+          })
+      end
     end
+    return users
   end
 
   def self.find(id)
-    results = DB.exec("SELECT * FROM users WHERE id=#{id};")
-    return {
-        "id" => results.first["id"].to_i,
-        "user_name" => results.first["user_name"],
-        "password" => results.first["password"],
-        "avatar" => results.first["avatar"]
-    }
-  end
-
-  def self.create(opts)
     results = DB.exec(
         <<-SQL
-            INSERT INTO users (user_name, password, avatar)
-            VALUES ('#{opts["user_name"]}', '#{opts["password"]}', '#{opts["avatar"]}')
-            RETURNING id, user_name, password, avatar;
+          SELECT *
+          FROM users
+          JOIN posts
+          ON users.id = posts.user_id
+          WHERE users.id =#{id};
         SQL
     )
     return {
         "id" => results.first["id"].to_i,
         "user_name" => results.first["user_name"],
         "password" => results.first["password"],
-        "avatar" => results.first["avatar"]
+        "avatar" => results.first["avatar"],
+        "post_id" => results.first["post_id"]
+    }
+  end
+
+  def self.create(opts)
+    results = DB.exec(
+        <<-SQL
+            INSERT INTO users (user_name, password, avatar, post_id)
+            VALUES ('#{opts["user_name"]}', '#{opts["password"]}', '#{opts["avatar"]}', '#{opts["post_id"]}')
+            RETURNING id, user_name, password, avatar, post_id;
+        SQL
+    )
+    return {
+        "id" => results.first["id"].to_i,
+        "user_name" => results.first["user_name"],
+        "password" => results.first["password"],
+        "avatar" => results.first["avatar"],
+        "post_id" => results.first["post_id"]
     }
   end
 
@@ -47,16 +84,17 @@ class User
     results = DB.exec(
         <<-SQL
             UPDATE users
-            SET user_name='#{opts["user_name"]}', password='#{opts["password"]}', avatar='#{opts["avatar"]}'
+            SET user_name='#{opts["user_name"]}', password='#{opts["password"]}', avatar='#{opts["avatar"]}', post_id=#{opts["post_id"]}
             WHERE id=#{id}
-            RETURNING id, user_name, password, avatar
+            RETURNING id, user_name, password, avatar, post_id
         SQL
     )
     return {
         "id" => results.first["id"].to_i,
         "user_name" => results.first["user_name"],
         "password" => results.first["password"],
-        "avatar" => results.first["avatar"]
+        "avatar" => results.first["avatar"],
+        "post_id" => results.first["post_id"]
     }
   end
 end
