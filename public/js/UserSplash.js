e1646213cb374da353d2c0388a99fc24c7ace4bb
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-08-12 16:24:59 -0400

    css styling for forms

--- public/js/UserSplash.js
+++ public/js/UserSplash.js
@@ -1,7 +1,7 @@
 class UserSplash extends React.Component {
   render() {
     return (
-      <div>
+      <div className="user_splash">
         <h1>This is the UserSplash section for {this.props.loggedUser.user_name}!</h1>
         <button className="button is-link" onClick={() => this.props.changePage("postForm")}>New Post</button>
       </div>
