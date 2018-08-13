c15dfdf2bc65b572e7a79262af1086e10ab692c0
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-08-13 13:46:43 -0400

    user show touched up with divs and individual posts

--- public/js/UserShow.js
+++ public/js/UserShow.js
@@ -4,21 +4,31 @@ class UserShow extends React.Component {
   render() {
     return (
       <div className="user_show">
-        <h1>Welcome to Quitter, {this.props.selectedUser.user_name}</h1>
-        <h2>Password: {this.props.selectedUser.password}</h2>
-        <img src={this.props.selectedUser.avatar}/>
-        {this.props.selectedUser.posts.map((post, index) => {
-          return (
-            <h1>{post.post_content}</h1>
-          )
-        })
-        }
-        {/* If the selectedUser is also the loggedUser then allow them to edit their user information */}
-        {
-          (this.props.selectedUser == this.props.loggedUser)?
-            <button className="button is-link" onClick={() => this.props.changePage("userEdit")}>Edit User</button>
-          : ''
-        }
+          <h1 className="user_welcome">Welcome to Quitter, {this.props.selectedUser.user_name}</h1>
+          <h2>Password: {this.props.selectedUser.password}</h2>
+          <img className="user_pic" src={this.props.selectedUser.avatar}/>
+          {this.props.selectedUser.posts.map((post, index) => {
+            return (
+              <div className="one_post">
+                <div className="content">
+                  {console.log({post})}
+                  <p>{post.post_content}</p>
+                  <img src={post.img}/>
+                </div>
+                <div className="user">
+                  <div className="avatar"><img src={post.avatar}/></div>
+                  <h3>{post.user_name}</h3>
+                </div>
+              </div>
+            )
+          })
+          }
+          {/* If the selectedUser is also the loggedUser then allow them to edit their user information */}
+          {
+            (this.props.selectedUser == this.props.loggedUser)?
+              <button className="button is-link" onClick={() => this.props.changePage("userEdit")}>Edit User</button>
+            : ''
+          }
       </div>
     )
   }
