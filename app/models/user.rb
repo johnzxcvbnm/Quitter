598d6819361c5d2e33434fa3d9cd915ec20bfc76
John Kusching <kimshrum@Kims-Air.home>
2018-08-14 10:57:40 -0500

    Updated users.rb to match dev branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,139 +1,145 @@
 class User
   DB = PG.connect({ :host => "localhost", :port => 5432, :dbname => 'quitter_app_development'})
   def self.all
-    results = DB.exec(
-        <<-SQL
-          SELECT users.*,
-          posts.id as postid,
-          posts.post_content as post_content,
-          posts.image as post_image,
-          posts.user_id as post_user_id,
-          comments.id as comment_id,
-          comments.comment_content as comment_content,
-          comments.image as comment_image,
-          comments.user_id as comment_user_id,
-          comments.post_id as comment_post_id
-          FROM users
-          LEFT JOIN posts
-            ON users.id = posts.user_id
-          LEFT JOIN comments
-            ON users.id = comments.user_id
-          ORDER BY users.id;
-        SQL
-    )
-    users = []
-    last_user_id = nil
-    results.each do |result|
-      posts = []
-      if result["id"] != last_user_id
-      new_user = {
-        "id" => result["id"].to_i,
-        "user_name" => result["user_name"],
-        "password" => result["password"],
-        "avatar" => result["avatar"],
-        "posts" => posts,
-        "comments" => []
-        }
-        users.push(new_user)
-        last_user_id = result["id"]
-      end
-      last_comment_id = nil
-      if result["comment_id"]
-        if result["comment_id"] != last_comment_id
-        users.last["comments"].push({
-          "id" => result["comment_id"].to_i,
-          "comment_content" => result["comment_content"],
+      results = DB.exec(
+          <<-SQL
+            SELECT users.*,
+            posts.id as postid,
+            posts.post_content as post_content,
+            posts.image as post_image,
+            posts.user_id as post_user_id,
+            comments.id as comment_id,
+            comments.comment_content as comment_content,
+            comments.image as comment_image,
+            comments.user_id as comment_user_id,
+            comments.post_id as comment_post_id
+            FROM users
+            LEFT JOIN posts
+              ON users.id = posts.user_id
+            LEFT JOIN comments
+              ON users.id = comments.user_id
+            ORDER BY users.id ASC;
+          SQL
+      )
+      users = []
+      last_user_id = nil
+      results.each do |result|
+        if result["id"] != last_user_id
+        new_user = {
+          "id" => result["id"].to_i,
           "user_name" => result["user_name"],
-          "image" => result["comment_image"]
-        })
+          "password" => result["password"],
+          "avatar" => result["avatar"],
+          "posts" => [],
+          "comments" => []
+          }
+          users.push(new_user)
+          last_user_id = result["id"]
+        end
+        posts = []
+        comments = []
+        last_comment_id = nil
+        if result["comment_id"]
+          puts result["comment_id"]
+          if result["comment_id"] != last_comment_id
+          new_comment = ({
+            "id" => result["comment_id"].to_i,
+            "comment_content" => result["comment_content"],
+            "user_name" => result["user_name"],
+            "image" => result["comment_image"]
+          })
+          users.last["comments"].push(new_comment)
+          last_comment_id = result["comment_id"]
+          end
+        end
+
+
+        last_post_id = nil
+          if result["postid"] != last_post_id
+            puts result["postid"]
+            users.last["posts"].push({
+            "id" => result["postid"].to_i,
+            "post_content" => result["post_content"],
+            "image" => result["post_image"],
+            "user_name" => result["user_name"]
+            })
+            last_post_id = result["postid"]
+
+        end
+      end
+      users.each do |user|
+        user["posts"] = user["posts"].uniq
+        user["comments"] = user["comments"].uniq
+      end
+      return users
+    end
+    def self.find(id)
+      results = DB.exec(
+          <<-SQL
+            SELECT users.*,
+            posts.id as post_id,
+            posts.post_content as post_content,
+            posts.image as post_image,
+            posts.user_id as post_user_id,
+            comments.id as comment_id,
+            comments.comment_content as comment_content,
+            comments.image as comment_image,
+            comments.user_id as comment_user_id,
+            comments.post_id as comment_post_id
+            FROM users
+            LEFT JOIN posts
+              ON posts.user_id = users.id
+            LEFT JOIN comments
+              ON users.id = comments.user_id
+            WHERE users.id =#{id};
+          SQL
+      )
+      posts = []
+      last_post_id = nil
+      results.each do |result|
+        if result["post_id"]
+          if result["post_id"] != last_post_id
+          posts.push({
+            "id" => result["post_id"].to_i,
+            "post_content" => result["post_content"],
+            "image" => result["post_image"],
+            "user_name" => result["user_name"]
+            })
+        end
+        last_post_id = result["post_id"]
+        end
+      end
+      comments = []
+      last_comment_id = nil
+      results.each do |result|
+        if result["comment_id"]
+          if result["comment_id"] != last_comment_id
+          comments.push({
+            "id" => result["comment_id"].to_i,
+            "comment_content" => result["comment_content"],
+            "image" => result["comment_image"],
+            "user_name" => result["user_name"]
+            })
+        end
         last_comment_id = result["comment_id"]
         end
       end
-
-
-      last_post_id = nil
-        if result["postid"] != last_post_id
-          posts.push({
-          "id" => result["postid"].to_i,
-          "post_content" => result["post_content"],
-          "image" => result["post_image"],
-          "user_name" => result["user_name"]
-          })
-          last_post_id = result["postid"]
-
-      end
+      return {
+          "id" => results.first["id"].to_i,
+          "user_name" => results.first["user_name"],
+          "password" => results.first["password"],
+          "avatar" => results.first["avatar"],
+          "post_id" => results.first["post_id"],
+          "posts" => posts,
+          "comments" => comments
+      }
     end
-    return users
-  end
-
-  def self.find(id)
-    results = DB.exec(
-        <<-SQL
-          SELECT users.*,
-          posts.id as post_id,
-          posts.post_content as post_content,
-          posts.image as post_image,
-          posts.user_id as post_user_id,
-          comments.id as comment_id,
-          comments.comment_content as comment_content,
-          comments.image as comment_image,
-          comments.user_id as comment_user_id,
-          comments.post_id as comment_post_id
-          FROM users
-          LEFT JOIN posts
-            ON posts.user_id = users.id
-          LEFT JOIN comments
-            ON users.id = comments.user_id
-          WHERE users.id =#{id};
-        SQL
-    )
-    posts = []
-    last_post_id = nil
-    results.each do |result|
-      if result["post_id"]
-        if result["post_id"] != last_post_id
-        posts.push({
-          "id" => result["post_id"].to_i,
-          "post_content" => result["post_content"],
-          "image" => result["post_image"],
-          "user_name" => result["user_name"],
-          "avatar" => result["avatar"]
-          })
-      end
-      last_post_id = result["post_id"]
-      end
-    end
-    comments = []
-    last_comment_id = nil
-    results.each do |result|
-      if result["comment_id"]
-        if result["comment_id"] != last_comment_id
-        comments.push({
-          "id" => result["comment_id"].to_i,
-          "comment_content" => result["comment_content"],
-          "image" => result["comment_image"],
-          "user_name" => result["user_name"]
-          })
-      end
-      last_comment_id = result["comment_id"]
-      end
-    end
-    return {
-        "id" => results.first["id"].to_i,
-        "user_name" => results.first["user_name"],
-        "password" => results.first["password"],
-        "avatar" => results.first["avatar"],
-        "post_id" => results.first["post_id"],
-        "posts" => posts,
-        "comments" => comments
-    }
-  end
   def self.create(opts)
     results = DB.exec(
         <<-SQL
-            INSERT INTO users (user_name, password, avatar)
-            VALUES ('#{opts["user_name"]}', '#{opts["password"]}', '#{opts["avatar"]}')
-            RETURNING id, user_name, password, avatar;
+            INSERT INTO users (user_name, password, avatar, post_id)
+            VALUES ('#{opts["user_name"]}', '#{opts["password"]}', '#{opts["avatar"]}', '#{opts["post_id"]}')
+            RETURNING id, user_name, password, avatar, post_id;
         SQL
     )
     return {
@@ -141,7 +147,7 @@ class User
         "user_name" => results.first["user_name"],
         "password" => results.first["password"],
         "avatar" => results.first["avatar"],
-
+        "post_id" => results.first["post_id"]
     }
   end
   def self.delete(id)
